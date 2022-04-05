2b6612948e999c5abe7790658db187ff91064bf9
Maal21maat <maat21@student.bth.se>
2022-04-05 09:26:08 +0100

    Uppstart mha Övning 4

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import warehouse from './assets/warehouse.jpg';
+import Stock from './components/Stock.tsx';
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.base}>
+          <Text style={{color: '#33c', fontSize: 42}}>Lager-Appen</Text>
+          <Image source={warehouse} style={{ width: 320, height: 240 }} />
+          <Stock/>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingLeft: 12,
+    paddingRight: 12,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,15 @@
+Kurs Webapp v4
+==============
+
+Kmom01 Uppstart
+
+Kmom02
+
+Kmom03
+
+Kmom04
+
+Kmom05
+
+Kmom06
+
