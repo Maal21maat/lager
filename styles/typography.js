2aaa3133a9642eb4722e6400d24917e118e51ece
Maal21maat <maat21@student.bth.se>
2022-04-23 16:18:14 +0100

    struktur för styling

---
+++ styles/typography.js
@@ -0,0 +1,33 @@
+export const header1 = {
+    fontSize: 42,
+    marginBottom: 40,
+    color: '#1e7694',
+};
+
+export const header2 = {
+    fontSize: 34,
+    marginBottom: 10,
+    color: '#1e7694',
+};
+
+export const header3 = {
+    fontSize: 28,
+    marginBottom: 28,
+    color: '#1e7694',
+};
+
+export const normal = {
+    fontSize: 20,
+    marginBottom: 20,
+};
+
+export const small = {
+    fontSize: 18,
+    marginBottom: 10,
+};
+
+export const warning = {
+    fontSize: 20,
+    marginBottom: 20,
+    color: 'red',
+};
